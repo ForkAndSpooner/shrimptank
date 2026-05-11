cbc2c68e0c08078fd85ee536e7b48ba0af839c4f
ForkAndSpooner <brianscottlevine@gmail.com>
2026-05-11 10:16:01 -0400

    feat: literal vs creative pitch mode

--- server/src/index.js
+++ server/src/index.js
@@ -24,10 +24,11 @@ io.on("connection", (socket) => {
   let currentRoom = null;
   let playerName = null;
 
-  socket.on("create-room", (name, vsAi, buzzMode, cb) => {
-    if (typeof vsAi === "function") { cb = vsAi; vsAi = false; buzzMode = false; }
-    else if (typeof buzzMode === "function") { cb = buzzMode; buzzMode = false; }
-    const room = createRoom(name, vsAi, buzzMode);
+  socket.on("create-room", (name, vsAi, buzzMode, pitchMode, cb) => {
+    if (typeof vsAi === "function") { cb = vsAi; vsAi = false; buzzMode = false; pitchMode = "literal"; }
+    else if (typeof buzzMode === "function") { cb = buzzMode; buzzMode = false; pitchMode = "literal"; }
+    else if (typeof pitchMode === "function") { cb = pitchMode; pitchMode = "literal"; }
+    const room = createRoom(name, vsAi, buzzMode, pitchMode);
     currentRoom = room.code;
     playerName = name;
     socket.join(room.code);
@@ -78,7 +79,7 @@ io.on("connection", (socket) => {
 
       const pitchPromises = updated.players.map(async p => {
         if (p.isAi) {
-          const { pitch, selections } = await generateAiOpponentPitch(updated.market, updated.hands[p.name], updated.buzzWord);
+          const { pitch, selections } = await generateAiOpponentPitch(updated.market, updated.hands[p.name], updated.buzzWord, updated.pitchMode);
           // Store AI selections
           selectCards(currentRoom, p.name, [
             updated.hands[p.name].indexOf(selections[0]),
@@ -87,7 +88,7 @@ io.on("connection", (socket) => {
           return { playerName: p.name, pitch };
         }
         const [c1, c2] = updated.selections[p.name];
-        const pitch = await generatePitch(updated.market, c1, c2, p.name, updated.buzzWord);
+        const pitch = await generatePitch(updated.market, c1, c2, p.name, updated.buzzWord, updated.pitchMode);
         return { playerName: p.name, pitch };
       });
 
